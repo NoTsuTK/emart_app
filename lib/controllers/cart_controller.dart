2eaf007b82525b5464b52e5ad36c64a985fe87ea
NoTsuTK <nattyvader2004@gmail.com>
2023-02-26 18:26:52 +0700

    complete

--- lib/controllers/cart_controller.dart
+++ lib/controllers/cart_controller.dart
@@ -17,6 +17,7 @@ class CartController extends GetxController {
 
   late dynamic productSnapshot;
   var products = [];
+  var vendors = [];
 
   var placeingOrder = false.obs;
 
@@ -53,7 +54,8 @@ class CartController extends GetxController {
       'order_delivered': false,
       'order_on_delivery': false,
       'total_amount': totalAmount,
-      'orders': FieldValue.arrayUnion(products)
+      'orders': FieldValue.arrayUnion(products),
+      'vendors': FieldValue.arrayUnion(vendors)
     });
 
     placeingOrder(false);
@@ -61,6 +63,7 @@ class CartController extends GetxController {
 
   getProductDetails() {
     products.clear();
+    vendors.clear();
     for (var i = 0; i < productSnapshot.length; i++) {
       products.add({
         'color': productSnapshot[i]['color'],
@@ -70,6 +73,7 @@ class CartController extends GetxController {
         'qty': productSnapshot[i]['qty'],
         'title': productSnapshot[i]['title'],
       });
+      vendors.add(productSnapshot[i]['vendor_id']);
     }
   }
 
